a70fde4346cea8cc039751278a5801d6998bb7d8
Alex Chu <alexchu414@gmail.com>
2024-03-10 04:39:03 -0400

    Remove basePath for vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const { parsed: myEnv } = dotenvConfig();
 
 const nextConfig = {
   output: "export",
-  basePath: '/wp1',
+  // basePath: '/wp1',
   images: { unoptimized: true },
   webpack(config) {
     config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
