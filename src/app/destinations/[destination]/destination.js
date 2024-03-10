c35ff00b631093fd968c992ffc44c2c7a699a858
Alex Chu <61229735+alexchu-dev@users.noreply.github.com>
2024-03-10 06:09:05 -0400

    Update destination.js

--- src/app/destinations/[destination]/destination.js
+++ src/app/destinations/[destination]/destination.js
@@ -43,7 +43,7 @@ export default function Destination({slug}) {
     left: "50%",
     transform: "translate(-50%, -50%)",
     width: "2/3",
-    minWidrh: "400px",
+    minWidth: "400px",
     maxWidth: "90%",
     bgcolor: "background.paper",
     borderRadius: 2,
